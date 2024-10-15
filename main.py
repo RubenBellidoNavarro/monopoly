dff2a02ba4d6159352bbe784c9e381fb5728cd2e
alejandroenti <alejandro.lopez@enti.cat>
2024-10-15 20:35:35 +0200

    Añadimos constantes y variables generales necesarias

--- main.py
+++ main.py
@@ -2,3 +2,78 @@
 # IES ESTEVE TERRADAS I ILLAS
 # DAM 2024-2025
 # RUBÉN BELLIDO Y ALEJANDRO LÓPEZ
+
+import sys
+import os
+
+MIN_DINERS_BANCA = 500000
+
+tauler = {}
+banca = 0
+casella_mesures = {
+    "alt": 2,
+    "ampla": 8
+}
+
+tauler_mesures = {
+    "ampla_total": 64,
+    "ampla_partida": 10,
+    "files": 7,
+    "columnes": 7
+}
+
+jugadors = {
+    "Vermell": {
+        "nom": "Vermell",
+        "icona": "V",
+        "diners": 0,
+        "posicio": [],
+        "propietats": [],
+        "es_preso": False,
+        "cartes": []
+    },
+    "Groc": {
+        "nom": "Groc",
+        "icona": "G",
+        "diners": 0,
+        "posicio": [],
+        "propietats": ["Gracia", "Pl Cat"],
+        "es_preso": False,
+        "cartes": []
+    },
+    "Taronja": {
+        "nom": "Taronja",
+        "icona": "T",
+        "diners": 0,
+        "posicio": [],
+        "propietats": ["Aribau"],
+        "es_preso": False,
+        "cartes": []
+    },
+    "Blau": {
+        "nom": "Blau",
+        "icona": "B",
+        "diners": 0,
+        "posicio": [],
+        "propietats": [],
+        "es_preso": False,
+        "cartes": ["Sortir preso"]
+    }
+}
+
+caselles = {
+    "Lauria": {
+        "nom_acortat": "Lauria",
+        "lloguer_casa": 10,
+        "lloguer_hotel": 15,
+        "preu_terreny": 50,
+        "comprar_casa": 300,
+        "comprar_hotel": 250
+    },
+}
+
+posicions_caselles_files = [1, 5, 8, 11, 14, 17, 19]
+posicions_caselles_columnes = [0, 10, 19, 28, 37, 45, 55]
+posicions_separadors = [[0, 4], [0, 22]]
+posicions_informacio = [[66, 1], [66, 4], [66, 9], [66, 14], [66, 19]]
+posicio_estat = [5, 11]

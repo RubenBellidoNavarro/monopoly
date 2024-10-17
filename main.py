96cce6afab662bc7114784f57901ec6465915d41
RubenBellidoNavarro <ruben.bellido.navarro@gmail.com>
2024-10-17 15:44:17 +0200

    he actualizado mi rama con el contenido de main

--- main.py
+++ main.py
@@ -3,10 +3,19 @@
 # DAM 2024-2025
 # RUBÉN BELLIDO Y ALEJANDRO LÓPEZ
 
+import sys
+import os
+from colorama import just_fix_windows_console # Paquete para que la terminal de Windows entienda los caràcteres ANSI y podamos mover el cursor a la posición que deseemos 
+
+just_fix_windows_console()
+
+#region VARIABLESYCONSTANTES
+MIN_DINERS_BANCA = 500000
+MAX_LINIES_JUGADES = 13
+
+
 import random
 import json
-
-MIN_DINERS_BANCA = 500000
 
 banca = 1000000
 
@@ -17,7 +26,9 @@ casella_mesures = {
 
 tauler_mesures = {
     "ampla_total": 64,
-    "ampla_partida": 10
+    "ampla_partida": 10,
+    "files": 7,
+    "columnes": 7
 }
 
 #Array que utilizaremos para generar un orden de tiradas, y el diccionario de jugadores:
@@ -103,16 +114,8 @@ caselles_ordenades_nom_acortat = ("Sortida",
                       "Gracia"
                       )
 
-
 #Tupla de casillas que no tienen asociadas precios ni pertenencias a ningún jugador:
 caselles_especials = ("Sort","Presó","Caixa","Anr pró","Sortida","Parking")
-
-#Tupla con los nombres completos de cada casilla, sin contar las casillas especiales:
-noms_complets = []
-for casella in caselles_ordenades:
-    if casella not in caselles_especials:
-        noms_complets.append(casella)
-noms_complets = tuple(noms_complets)
 
 #Tupla que contiene la utilización de los precios para cada casilla junto a su nombre acortado.
 #Las posiciones de cada tupla corresponden a:
@@ -145,7 +148,29 @@ etiquetes_preus_caselles = ("nom_acortat",
                             "preu_terreny",
                             "comprar_casa",
                             "comprar_hotel")
-                  
+
+posicions_caselles_files = [1, 5, 8, 11, 14, 17, 19]
+posicions_caselles_columnes = [0, 10, 19, 28, 37, 45, 55]
+posicions_separadors = [[0, 4], [0, 22]]
+posicions_informacio = [[66, 1], [66, 4], [66, 9], [66, 14], [66, 19]]
+posicio_jugades = [5, 11]
+#endregion VARIABLESYCONSTANTES
+
+#region FuncionesInteraccionConsola
+def clearScreen():
+    if os.name == 'nt':     # Si estàs a Windows
+        os.system('cls')
+    else:                   # Si estàs a Linux o macOS
+        os.system('clear')
+
+def mou_cursor(x, y):
+    # Escapamos y nos movemos a la posición indicada
+    sys.stdout.write(f"\033[{y};{x}H")
+    # Forzamos un aplicado del buffer
+    sys.stdout.flush()
+#endregion FuncionesInteraccionConsola
+
+#region GeneracionPartida             
 def genera_jugadors(noms_jugadors:list) -> dict:
     '''Genera un diccionario de jugadores, donde cada clave será el nombre 
     del jugador (Ex. "Vermell") y su valor será un diccionario con información 
@@ -243,6 +268,30 @@ def genera_tauler(caselles_ordenades, caselles_ordeandes_nom_acortat, caselles_e
 
     return tauler
 
+def primer_pagament(jugadors):
+    '''Añadimos a todos los jugadores 2000€ en sus cuentas.
+
+    Input:
+        -jugadors(dict): Diccionario en el que cada clave es un jugador, y su valor asociado es un diccionario con información del jugador (icona, diners, posicio, propietats, es_preso, cartes)
+        
+    Retorna: No retorna nada'''
+    for jugador in jugadors:
+        jugadors[jugador]["diners"] = 2000
+
+def ordre_tirada(jugadors):
+    '''Mezaclamos el diccionario de jugadores para que tengan un orden aleatorio
+    
+    Input:
+        -jugadors(dict): Diccionario en el que cada clave es un jugador, y su valor asociado es un diccionario con información del jugador (icona, diners, posicio, propietats, es_preso, cartes)
+        
+    Retorna:
+        -ordre_jugadors(list): Lista que contiene los nombres de los jugadores mezclados aleatoriamente. Esta lista marcará el orden de tiradas durante la partida.'''
+    ordre_jugadors = list(jugadors.keys())
+    random.shuffle(ordre_jugadors)
+    return ordre_jugadors
+#endregion GeneracionPartida
+
+#region GestioBanca
 def afegir_diners_banca(banca):
     '''Añadimos dinero a la banca cuando esta no disponga de suficiente dinero (< 500.000€).
     
@@ -261,28 +310,321 @@ def gestiona_diners_banca(banca:int):
     Retorna: No retorna nada'''
     if banca < MIN_DINERS_BANCA:
         afegir_diners_banca()
+#endregion GestioBanca
 
-def ordre_tirada(jugadors):
-    '''Mezaclamos el diccionario de jugadores para que tengan un orden aleatorio
-    
-    Input:
-        -jugadors(dict): Diccionario en el que cada clave es un jugador, y su valor asociado es un diccionario con información del jugador (icona, diners, posicio, propietats, es_preso, cartes)
-        
-    Retorna:
-        -ordre_jugadors(list): Lista que contiene los nombres de los jugadores mezclados aleatoriamente. Esta lista marcará el orden de tiradas durante la partida.'''
-    ordre_jugadors = list(jugadors.keys())
-    random.shuffle(ordre_jugadors)
-    return ordre_jugadors
+#region ImpresionTablero
+def imprimeix_separador():
+    amplada = casella_mesures["ampla"]
+    print(f"+{"".ljust(amplada, "-")}+{"".ljust(amplada, "-")}+{"".ljust(amplada, "-")}+{"".ljust(amplada, "-")}+{"".ljust(amplada, "-")}+{"".ljust(amplada, "-")}+{"".ljust(amplada, "-")}+")
 
-def primer_pagament(jugadors):
-    '''Añadimos a todos los jugadores 2000€ en sus cuentas.
+def imprimeix_casella_vertical(nom, cases, hotels, jugadors, posicio):
+    amplada = casella_mesures["ampla"]
+    final_linia = "|"
+    separador = f"+{"".ljust(amplada, "-")}+"
+
+    string_final = f"{final_linia}"
+    if cases > 0:
+        amplada -= 1
+        final_linia = f"{cases}C"
+
+    string_final += f"{nom.ljust(amplada)}{final_linia}"
+    mou_cursor(posicio[1], posicio[0])
+    print(string_final)
+
+    amplada = casella_mesures["ampla"]
+    final_linia = "|"
+    if hotels > 0:
+        amplada -= 1
+        final_linia = f"{hotels}H"
+    string_final = "|"
+    if len(jugadors) != 0:
+        string_jugadors = ""
+        for jugador in jugadors:
+            string_jugadors += jugador
+        string_final += f"{string_jugadors.ljust(amplada)}"
+    else:
+        string_final += f"{"".ljust(amplada)}"
+    string_final += f"{final_linia}"
+    mou_cursor(posicio[1], posicio[0] + 1)
+    print(string_final)
     
-    Input:
-        -jugadors(dict): Diccionario en el que cada clave es un jugador, y su valor asociado es un diccionario con información del jugador (icona, diners, posicio, propietats, es_preso, cartes)
+    if posicio[0] != 17:
+        string_final = f"{separador}"
+        mou_cursor(posicio[1], posicio[0] + 2)
+        print(string_final)
+
+def imprimeix_casella_horizontal(nom, cases, hotels, jugadors, posicio):
+    primera_linia = "+"
+    if cases > 0:
+        primera_linia += f"{"".ljust(4, "-")}{cases}C"
+    else:
+        primera_linia += f"".ljust(6, "-")
+
+    if hotels > 0:
+        primera_linia += f"{hotels}H+"
+    else:
+        primera_linia += f"".ljust(2, "-") + "+"
+    
+    amplada = casella_mesures["ampla"]
+    final_linia = "|"
+    if len(jugadors) != 0:
+            string_jugadors = ""
+            for jugador in jugadors:
+                string_jugadors += jugador
+            string_jugadors = string_jugadors.ljust(amplada)
+    else:
+        string_jugadors = "".ljust(amplada)
+
+    if posicio[0] == 19:
+        mou_cursor(posicio[1], posicio[0])
+        print(primera_linia)
+
+        mou_cursor(posicio[1], posicio[0] + 1)
+        print(f"{final_linia}{string_jugadors}{final_linia}")
+
+        mou_cursor(posicio[1], posicio[0] + 2)
+        print(f"{final_linia}{nom.ljust(amplada)}{final_linia}")
+
+    elif posicio[0] == 1:
+        mou_cursor(posicio[1], posicio[0])
+        print(primera_linia)
+
+        mou_cursor(posicio[1], posicio[0] + 1)
+        print(f"{final_linia}{nom.ljust(amplada)}{final_linia}")
+
+        mou_cursor(posicio[1], posicio[0] + 2)
+        print(f"{final_linia}{string_jugadors}{final_linia}")
+
+
+def imprimeix_casella(nom, cases, hotels, jugadors, posicio):
+    # Imprimimos por pantalla la casilla 
+    # Gestionamos:
+    #   - Impresión del nombre
+    #   - Impresión del número de casas y hoteles
+    #   - Impresión de los jugadores en la casilla
+    # Miramos si la casilla debe tener la información en horizontal o en vertical
+    if posicio[0] != 1 and posicio[0] != 19:
+        imprimeix_casella_vertical(nom, cases, hotels, jugadors, posicio)
+    else:
+        imprimeix_casella_horizontal(nom, cases, hotels, jugadors, posicio)
+
+def imprimeix_fila(fila_caselles):
+    # Recibimos una fila del tablero.
+    # Iteramos por la fila e imprimimos cada una de las casillas de la fila
+    for casella in fila_caselles:
+            imprimeix_casella(casella["nom_acortat"], casella["cases"], casella["hotels"], casella["jugadors"], casella["posicio"])
+
+def imprimeix_taula(tauler):
+    # Imprimimos del tablero, llamando cada de las fila con los separadores
+    for index in posicions_caselles_files:
+        fila = list(filter(lambda casella: casella["posicio"][0] == index, tauler))
+        imprimeix_fila(fila)
+    
+    mou_cursor(posicions_separadors[0][0], posicions_separadors[0][1])
+    imprimeix_separador()
+    mou_cursor(posicions_separadors[1][0], posicions_separadors[1][1])
+    imprimeix_separador()
+#endregion ImpresionTablero
+
+#region ImprimirInformacion
+def imprimeix_informacio_banca(banca):
+    # Imprimimos la información de la banca
+    #   Banca:
+    #   Diners: 1838734
+    mou_cursor(posicions_informacio[0][0], posicions_informacio[0][1])
+    print("Banca: ")
+    mou_cursor(posicions_informacio[0][0], posicions_informacio[0][1] + 1)
+    print(f"Diners: {banca}")
+
+def imprimeix_informacio_jugador(index, jugador):
+    # Imprimimos la información de la banca
+    #   Jugador Groc:
+    #   Carrers: 2834
+    #   Diners: 1838734
+    #   Especial: (res) "Nombre de las cartas especiales"
+    mou_cursor(posicions_informacio[index][0], posicions_informacio[index][1])
+    print(f"Jugador {jugador["nom"]}: ")
+
+    mou_cursor(posicions_informacio[index][0], posicions_informacio[index][1] + 1)
+    print(f"Carrers: ", end="")
+    longitud = len(jugador["propietats"])
+    if longitud != 0:
+        for index_, propietat in enumerate(jugador["propietats"]):
+            print(f"{propietat}", end="")
+            if index_ != longitud - 1:
+                print(", ", end="")
+
+    else:
+        print("(res)")
+
+    mou_cursor(posicions_informacio[index][0], posicions_informacio[index][1] + 2)
+    print(f"Diners: {jugador["diners"]} ")
+
+    mou_cursor(posicions_informacio[index][0], posicions_informacio[index][1] + 3)
+    print(f"Especial: ", end="")
+    longitud = len(jugador["cartes"])
+    if longitud != 0:
+        for index_, carta in enumerate(jugador["cartes"]):
+            print(f"{carta}", end="")
+            if index_ != longitud - 1:
+                print(", ", end="")
+    else:
+        print("(res)")  
+
+def imprimeix_informacio(banca, jugadors):
+    # Gestiona la impresión a la izquierda del tablero
+    # ¡MIRAR COMO CAMBIAR LA POSICIÓN DEL PUNTERO DE CONSOLA!
+    imprimeix_informacio_banca(banca)
+
+    for index, jugador in enumerate(jugadors.values()):
+        imprimeix_informacio_jugador(index + 1, jugador)
+#endregion ImprimirInformacion
+
+#region ImprimirJugadas
+def imprimeix_jugades(accions):
+    # Imprimimos en el espacio central del tablero 13 líneas de acciones realizadas por los jugadores
+    amplada = tauler_mesures["ampla_total"] - (tauler_mesures["ampla_partida"] * 2)
+    posicio_x = posicio_jugades[1]
+    posicio_y = posicio_jugades[0]
+    text_inici_accio = "Juga"
+    simbol_inici_accio = ">"
+
+    if len(accions) > MAX_LINIES_JUGADES:
+        min = len(accions) - MAX_LINIES_JUGADES
+        accions = accions[min:]
+    
+    for accio in accions:
+        # Recortamos el string en caso que no nos vaya a caber en el espacio establecido
+        if len(accio) > amplada:
+            min = len(accio) - amplada
+            accio = accio[min:]
+
+        # Movemos el cursor a la posición deseada e imprimimos la línea
+        # En caso que empecemos turno, deberemos añadir el carácter '>' al inicio
+        mou_cursor(posicio_x, posicio_y)
+        if text_inici_accio in accio:
+            print(f"{simbol_inici_accio} ", end="")
+            print(accio)
+        else:
+            print(f"  {accio}")
         
-    Retorna: No retorna nada'''
-    for jugador in jugadors:
-        jugadors[jugador]["diners"] = 2000
+        # Aumentamos en 1 la línea
+        posicio_y += 1
+#endregion ImprimirJugadas
+
+#region Joc
+
+#endregion Joc
+
+#region MAIN
+clearScreen()
+caselles = [
+    {
+        "nom_acortat": "Gracia",
+        "cases": 2,
+        "hotels": 0,
+        "jugadors": ["V"],
+        "posicio": [19, 10]
+    },
+    {
+        "nom_acortat": "Marina",
+        "cases": 1,
+        "hotels": 1,
+        "jugadors": [],
+        "posicio": [19, 0]
+    },
+    {
+        "nom_acortat": "Aribau",
+        "cases": 0,
+        "hotels": 2,
+        "jugadors": ["VGB"],
+        "posicio": [17, 0]
+    },
+    {
+        "nom_acortat": "Mallorca",
+        "cases": 0,
+        "hotels": 0,
+        "jugadors": ["B"],
+        "posicio": [5, 0]
+    },
+    {
+        "nom_acortat": "Arago",
+        "cases": 1,
+        "hotels": 1,
+        "jugadors": ["T"],
+        "posicio": [1, 0]
+    },
+    {
+        "nom_acortat": "Pl Cat",
+        "cases": 4,
+        "hotels": 2,
+        "jugadors": ["TB"],
+        "posicio": [1, 55]
+    },
+    {
+        "nom_acortat": "Sants",
+        "cases": 2,
+        "hotels": 0,
+        "jugadors": [""],
+        "posicio": [5, 55]
+    },
+]
+
+jugades = [
+    "Juga \"B\", ha sortit 4 i 3",
+    "\"B\" avança fins \"Aribau\"",
+    "\"B\" compra el terreny",
+    "Torn del jugador \"T\"",
+    "Juga \"T\", ha sortit 2 i 2",
+    "\"T\" surt de la pressó",
+    "\"T\" avança fins \"S.Joan\"",
+    "\"T\" paga ??€ de lloguer a \"V\"",
+    "Juga \"G\", ha sortit 1 i 1"
+    "Sort: Anar tres espais enrrera",
+    "\"G\" guanya 200€",
+    "Juga \"B\", ha sortit 4 i 2",
+    "\"B\" és a la pressó, 3 torns sense tirar",
+    "Juga \"B\", ha sortit 2 i 1",
+    "\"B\" avança fins \"Gracia\"",
+]
+
+clearScreen()
+imprimeix_taula(caselles)
+imprimeix_informacio(banca, jugadors)
+imprimeix_jugades(jugades)
+mou_cursor(0, 25)
+#endregion MAIN
+
+################################################RUBÉN
+
+
+# MONTAR DENTRO DE UNA FUNCIÓN (SE LLAMA AL INICIAR EL PROGRAMA)
+#Tupla con los nombres completos de cada casilla, sin contar las casillas especiales:
+noms_complets = []
+for casella in caselles_ordenades:
+    if casella not in caselles_especials:
+        noms_complets.append(casella)
+noms_complets = tuple(noms_complets)
 
 def main():
+    # Generar la partida
+    #   - Generamos el tablero
+    #       · Generamos las casillas y las metemos en el tablero
+    #   - Determinamos el orden de los jugadores
+    #   - Generamos los jugadores con los datos iniciales
+    #       · Les damos el primer ingreso
+    #   - Añandimos a la casilla 'Salida' todos los jugadores
+    # Iniciar bucle de juego
+    #   - Imprimir el tablero y la información
+    #   - Tiramos dados del jugador
+    #   - Actualizamos posición en tablero (borramos actual y ponemos la nueva, tanto en jugador como en casilla)
+    #   - Añadimos jugada a la lista de juagdas
+    #   - Volvemos a imprimir tablero e información con la nueva jugada
+    #   - Revisamos qué opciones tiene el usuario según la casilla en la que se encuentra
+    #       · En caso que no tenga opción (sólo puede pasar) saltar a siguiente jugador
+    #   - Gestionamos Input del jugador y realizamos las acciones correspondientes
+    #   - Revisar si jugador está quebrado (Dinero menor o igual a 0) y quitarlo de la lista de jugadores
+    #       · Eliminar de la lista de jugadores
+    #       · Eliminar del tablero
+    #   - Revisar si hay ganador para finalizar partida
     pass
